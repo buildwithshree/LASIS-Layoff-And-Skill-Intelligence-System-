c3b93742ec38ebd00b6c100c6fe956508452e359
Shreejal Mehta <igromeox26@gmail.com>
2026-03-29 23:38:15 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -6,7 +6,7 @@
 -- TABLE 1: departments
 CREATE TABLE IF NOT EXISTS departments (
   department_id  SERIAL PRIMARY KEY,
-  dept_name      VARCHAR(100) NOT NULL UNIQUE,
+  dept_name      VARCHAR(100) NOT NULL UNIQUE
   dept_code      VARCHAR(10)  NOT NULL UNIQUE,
   created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
